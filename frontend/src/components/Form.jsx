4287a7f0332397861e82e686fd681974b0fa063c
Emery Li <71569536+Emeryli@users.noreply.github.com>
2024-09-23 15:18:01 -0700

    Improve Login UI

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -12,6 +12,8 @@ function Form({ route, method }) {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [isDisabled, setDisable] = useState(true);
+  const [shake, setShake] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const name = method === "login" ? "Login" : "Register";
 
   const handleSubmit = async (e) => {
@@ -27,7 +29,15 @@ function Form({ route, method }) {
         navigate("/login");
       }
     } catch (error) {
-      alert(error);
+      setShake(true);
+      if (name == "Login") {
+        setErrorMessage("Username and password don't match");
+      } else {
+        setErrorMessage("Username exists");
+      }
+      setTimeout(() => {
+        setShake(false);
+      }, 500);
     } finally {
       setLoading(false);
     }
@@ -47,31 +57,50 @@ function Form({ route, method }) {
     <form onSubmit={handleSubmit} className="form-container">
       <h1>{name}</h1>
       <input
-        className="form-input"
+        className={`form-input ${shake ? "error" : ""}`}
         type="text"
         value={username}
         onChange={(e) => {
           setUsername(e.target.value);
+          setShake(false);
+          setErrorMessage('');
         }}
         placeholder="Username"
       />
       <input
-        className="form-input"
+        className={`form-input ${shake ? "error" : ""}`}
         type="password"
         value={password}
         onChange={(e) => {
           setPassword(e.target.value);
+          setShake(false);
+          setErrorMessage('');
+
         }}
         placeholder="Password"
       />
-      { name  == "Login" ? (
-        <Link to="/register" className="link">Doesn't have a account? Register Now!</Link>
+      {errorMessage && name == "Login" ? (
+        <p className="error-message">{errorMessage}</p>
       ) : (
-        <Link to="/login" className="link">Have a account? Login Now!</Link>
+        ""
+      )}
+      {errorMessage && name == "Register" ? (
+        <p className="error-message">{errorMessage}</p>
+      ) : (
+        ""
+      )}
+      {name == "Login" ? (
+        <Link to="/register" className="link">
+          Doesn't have a account? Register Now!
+        </Link>
+      ) : (
+        <Link to="/login" className="link">
+          Have a account? Login Now!
+        </Link>
       )}
 
       {loading && <LoadingIndicator />}
-      
+
       <button className="form-button" type="submit" disabled={isDisabled}>
         {name}
       </button>
